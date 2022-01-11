47765b4ba2ce36f0506520d6ca240578c39d59a4
Illyes Zerga <90515083+Elfamozoo@users.noreply.github.com>
2022-01-11 23:28:54 +0100

    Sidebar en cours

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,38 +1,50 @@
 import React from 'react';
 import "../styles/Navbar.scss";
-import Navbar from 'react-bootstrap/Navbar'
-import Nav from 'react-bootstrap/Nav'
+import { Navbar, Nav, Offcanvas, Container } from 'react-bootstrap';
 import rsburger from '../assets/rsburger.png'
+import Accueil from './Accueil';
 
 
 
 const Navibar = () => {
+
     return (
         <>
-            <Navbar fixed='top' bg="dark" variant="dark">
-                <Navbar.Brand href="#home">
-                    <img
-                        alt=""
-                        src={rsburger}
-                        width="80"
-                        height="50"
-                        style={{ marginLeft: "15px" }}
-                        className="d-inline-block align-top" />
-                </Navbar.Brand>
+            <Navbar sticky='top' bg="dark" variant="dark" expand={false}>
+                <Container fluid>
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src={rsburger}
+                            width="80"
+                            height="50"
+                            style={{ marginLeft: "15px" }}
+                            className="d-inline-block align-top" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                    <Navbar.Offcanvas
+                        id="offcanvasNavbar"
+                        aria-labelledby="offcanvasNavbarLabel"
+                        placement="end"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title id="offcanvasNavbarLabel">Menu</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav variant='dark' bg="dark" className="justify-content-end flex-grow-1 pe-3">
+                                <Nav.Link href="#action1">Accueil</Nav.Link>
+                                <Nav.Link href="#action2">Restaurant</Nav.Link>
+                                <Nav.Link href="#action2">Carte</Nav.Link>
+                                <Nav.Link href="#action2">Commander</Nav.Link>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
             </Navbar>
-            <Nav defaultActiveKey="/home" className="flex-column">
-                <Nav.Link href="/home">Active</Nav.Link>
-                <Nav.Link eventKey="link-1">Link</Nav.Link>
-                <Nav.Link eventKey="link-2">Link</Nav.Link>
-                <Nav.Link eventKey="disabled" disabled>
-                    Disabled
-                </Nav.Link>
-            </Nav>
 
 
         </>
     )
-
 };
 
 export default Navibar;
