1cff7bde0bfcac4db6b3e54b85bf4d55102ef562
Illyes Zerga <90515083+Elfamozoo@users.noreply.github.com>
2022-01-13 15:46:35 +0100

    Ajout Google Map

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,7 +12,7 @@ const Navibar = () => {
         <>
             <Navbar sticky='top' bg="dark" variant="dark" expand={false}>
                 <Container fluid>
-                    <Navbar.Brand href="Accueil">
+                    <Navbar.Brand href="/">
                         <img
                             alt=""
                             src={rsburger}
@@ -32,7 +32,7 @@ const Navibar = () => {
                         </Offcanvas.Header>
                         <Offcanvas.Body style={{ backgroundColor: "black" }}>
                             <Nav className="justify-content-end flex-grow-1 pe-3">
-                                <Nav.Link style={{ color: "white" }} href="accueil">Accueil</Nav.Link>
+                                <Nav.Link style={{ color: "white" }} href="/">Accueil</Nav.Link>
                                 <Nav.Link style={{ color: "white" }} href="restaurant">Restaurant</Nav.Link>
                                 <Nav.Link style={{ color: "white" }} href="carte">Carte</Nav.Link>
                                 <Nav.Link style={{ color: "white" }} href="commander">Commander</Nav.Link>
--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -1,9 +1,14 @@
 import React from 'react';
 
+
 class Restaurant extends React.Component {
+
     render() {
         return (
             <>
+                <div className='google-map'>
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d502.4007306551175!2d3.065083387733846!3d50.60963599385257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7a5e389e5b1a4036!2sRS%20Burger!5e0!3m2!1sfr!2sfr!4v1642083753686!5m2!1sfr!2sfr" width="100%" height="450" frameborder="0" style={{ border: 0 }} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+                </div>
 
             </>
         );
