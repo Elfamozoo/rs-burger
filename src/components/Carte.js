8cb245b30fb67097d2059f8c3cf0cc3a6f3efba8
Illyes Zerga <90515083+Elfamozoo@users.noreply.github.com>
2022-01-17 14:48:31 +0100

    CSS +Responsive Carte terminé

--- src/components/Carte.js
+++ src/components/Carte.js
@@ -12,59 +12,59 @@ class Carte extends React.Component {
                 <Container>
                     <Row xs={1} md={2} className="g-4">
                         {menu.map((products) =>
-
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={products.Image} />
-                                <Card.Body>
-                                    <Card.Title>{products.Nom}</Card.Title>
-                                    <Card.Text>
-                                        {products.Ingredients}
-                                    </Card.Text>
-                                    <Card.Text>
-                                        {products.Prix}
-                                    </Card.Text>
-                                    <Button variant="primary">Voir details</Button>
-                                </Card.Body>
-                            </Card>
-
+                            <Col xl={3}>
+                                <Card>
+                                    <Card.Img variant="top" src={products.Image} />
+                                    <Card.Body>
+                                        <Card.Title>{products.Nom}</Card.Title>
+                                        <Card.Text>
+                                            {products.Ingredients}
+                                        </Card.Text>
+                                        <Card.Text>
+                                            {products.Prix}
+                                        </Card.Text>
+                                        <Button variant="primary">Voir details</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
                         )}
                     </Row>
                     <Row xs={1} md={2} className="g-4">
                         {burger.map((products) =>
-
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={products.Image} />
-                                <Card.Body>
-                                    <Card.Title>{products.Nom}</Card.Title>
-                                    <Card.Text>
-                                        {products.Ingredients}
-                                    </Card.Text>
-                                    <Card.Text>
-                                        {products.Prix}
-                                    </Card.Text>
-                                    <Button variant="primary">Voir details</Button>
-                                </Card.Body>
-                            </Card>
-
+                            <Col xl={3}>
+                                <Card>
+                                    <Card.Img variant="top" src={products.Image} />
+                                    <Card.Body>
+                                        <Card.Title>{products.Nom}</Card.Title>
+                                        <Card.Text>
+                                            {products.Ingredients}
+                                        </Card.Text>
+                                        <Card.Text>
+                                            {products.Prix}
+                                        </Card.Text>
+                                        <Button variant="primary">Voir details</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
                         )}
                     </Row>
                     <Row xs={1} md={2} className="g-4">
                         {desserts.map((products) =>
-
-                            <Card style={{ width: '18rem' }}>
-                                <Card.Img variant="top" src={products.Image} />
-                                <Card.Body>
-                                    <Card.Title>{products.Nom}</Card.Title>
-                                    <Card.Text>
-                                        {products.Ingredients}
-                                    </Card.Text>
-                                    <Card.Text>
-                                        {products.Prix}
-                                    </Card.Text>
-                                    <Button variant="primary">Voir details</Button>
-                                </Card.Body>
-                            </Card>
-
+                            <Col xl={3}>
+                                <Card>
+                                    <Card.Img variant="top" src={products.Image} />
+                                    <Card.Body>
+                                        <Card.Title>{products.Nom}</Card.Title>
+                                        <Card.Text>
+                                            {products.Ingredients}
+                                        </Card.Text>
+                                        <Card.Text>
+                                            {products.Prix}
+                                        </Card.Text>
+                                        <Button variant="primary">Voir details</Button>
+                                    </Card.Body>
+                                </Card>
+                            </Col>
                         )}
                     </Row>
                 </Container>
