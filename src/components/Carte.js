e3678e7280d65e94fc5e58a5df7df30d95c347d4
Illyes Zerga <90515083+Elfamozoo@users.noreply.github.com>
2022-01-17 14:29:54 +0100

    Appel base de données reussi
    CSS a compléter + ajouts de categories a faire

--- src/components/Carte.js
+++ src/components/Carte.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Card, Container, Row, Button, Col } from 'react-bootstrap'
-import data from '../data/menu-burger.json'
+import menu from '../data/menu-burger.json'
+import burger from '../data/Burger.json'
+import desserts from '../data/accompagnements.json'
 
 
 class Carte extends React.Component {
@@ -9,7 +11,45 @@ class Carte extends React.Component {
             <>
                 <Container>
                     <Row xs={1} md={2} className="g-4">
-                        {data.map((products) =>
+                        {menu.map((products) =>
+
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={products.Image} />
+                                <Card.Body>
+                                    <Card.Title>{products.Nom}</Card.Title>
+                                    <Card.Text>
+                                        {products.Ingredients}
+                                    </Card.Text>
+                                    <Card.Text>
+                                        {products.Prix}
+                                    </Card.Text>
+                                    <Button variant="primary">Voir details</Button>
+                                </Card.Body>
+                            </Card>
+
+                        )}
+                    </Row>
+                    <Row xs={1} md={2} className="g-4">
+                        {burger.map((products) =>
+
+                            <Card style={{ width: '18rem' }}>
+                                <Card.Img variant="top" src={products.Image} />
+                                <Card.Body>
+                                    <Card.Title>{products.Nom}</Card.Title>
+                                    <Card.Text>
+                                        {products.Ingredients}
+                                    </Card.Text>
+                                    <Card.Text>
+                                        {products.Prix}
+                                    </Card.Text>
+                                    <Button variant="primary">Voir details</Button>
+                                </Card.Body>
+                            </Card>
+
+                        )}
+                    </Row>
+                    <Row xs={1} md={2} className="g-4">
+                        {desserts.map((products) =>
 
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src={products.Image} />
