e47236aac6358547396f144dcc8528bd964ec6f5
Illyes Zerga <90515083+Elfamozoo@users.noreply.github.com>
2022-01-17 16:37:54 +0100

    Onglets de nav sur carte a faire

--- src/components/Carte.js
+++ src/components/Carte.js
@@ -1,8 +1,11 @@
 import React from 'react';
-import { Card, Container, Row, Button, Col } from 'react-bootstrap'
+import { Card, Container, Row, Button, Col, Nav } from 'react-bootstrap'
 import menu from '../data/menu-burger.json'
 import burger from '../data/Burger.json'
 import desserts from '../data/accompagnements.json'
+import banmenu from '../assets/banniere_menu.png'
+import banburger from '../assets/banniere_burger.png'
+import banaccom from '../assets/banniere_accom.png'
 
 
 class Carte extends React.Component {
@@ -10,6 +13,26 @@ class Carte extends React.Component {
         return (
             <>
                 <Container>
+                    <Row xs={1} md={2}>
+                        <Nav fill variant="tabs" defaultActiveKey="/home">
+                            <Nav.Item>
+                                <Nav.Link eventKey="link-1" >Menus burgers/wraps</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="link-2">Burger/Wraps</Nav.Link>
+                            </Nav.Item>
+                            <Nav.Item>
+                                <Nav.Link eventKey="link-3" >Accompagnements</Nav.Link>
+                            </Nav.Item>
+                        </Nav>
+                    </Row>
+                    <Row xs={1} md={2}>
+                        <Card.Body>
+                            <Card.Title>
+                                Menus burgers / wraps
+                            </Card.Title>
+                        </Card.Body>
+                    </Row>
                     <Row xs={1} md={2} className="g-4">
                         {menu.map((products) =>
                             <Col xl={3}>
@@ -23,11 +46,20 @@ class Carte extends React.Component {
                                         <Card.Text>
                                             {products.Prix}
                                         </Card.Text>
-                                        <Button variant="primary">Voir details</Button>
+                                        <Button variant="dark">Voir details</Button>
                                     </Card.Body>
                                 </Card>
                             </Col>
                         )}
+                    </Row>
+                    <Row xs={1} md={2} className="g-0">
+
+                        <Card.Body>
+                            <Card.Title>
+                                Burger / Wraps
+                            </Card.Title>
+                        </Card.Body>
+
                     </Row>
                     <Row xs={1} md={2} className="g-4">
                         {burger.map((products) =>
@@ -42,11 +74,18 @@ class Carte extends React.Component {
                                         <Card.Text>
                                             {products.Prix}
                                         </Card.Text>
-                                        <Button variant="primary">Voir details</Button>
+                                        <Button variant="dark">Voir details</Button>
                                     </Card.Body>
                                 </Card>
                             </Col>
                         )}
+                    </Row>
+                    <Row xs={1} md={2} className="g-0 w-100">
+                        <Card.Body>
+                            <Card.Title>
+                                Accompagnements
+                            </Card.Title>
+                        </Card.Body>
                     </Row>
                     <Row xs={1} md={2} className="g-4">
                         {desserts.map((products) =>
@@ -61,7 +100,7 @@ class Carte extends React.Component {
                                         <Card.Text>
                                             {products.Prix}
                                         </Card.Text>
-                                        <Button variant="primary">Voir details</Button>
+                                        <Button variant="dark">Voir details</Button>
                                     </Card.Body>
                                 </Card>
                             </Col>
---
+++ src/components/Details.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Card, Container, Row, Button, Col } from 'react-bootstrap'
+
+class Details extends Component {
+    state = {}
+    render() {
+        return ();
+    }
+}
+
+export default Details;
