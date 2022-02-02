2b8f9fa5df8ad7d57b8a63b4badec8b3ef17c478
Illyes Zerga <90515083+Elfamozoo@users.noreply.github.com>
2022-02-02 13:29:43 +0100

    Page Restaurant complétée
    Image ajouté

--- src/components/Restaurant.js
+++ src/components/Restaurant.js
@@ -6,6 +6,7 @@ import { faStar, faStarHalfAlt, } from "@fortawesome/free-solid-svg-icons";
 import { faStar as farStar } from "@fortawesome/free-regular-svg-icons";
 import google from '../assets/logo-google.png'
 import justeat from '../assets/Just-Eat-Logo.png'
+import rsburger from '../assets/rsburgermap5.png'
 
 
 const icons = {
@@ -41,12 +42,13 @@ class Restaurant extends React.Component {
                             <a href='https://www.just-eat.fr/menu/rs-burger#avis'>+110 avis</a>
                         </Col>
                     </Row>
-
-                    <h1 className='descriptionH1'>Notre <strong>restaurant</strong> de <strong>burgers</strong> à <strong>Faches-Thumesnil</strong></h1>
-                    <h2 className='descriptionH2'>36 rue Ferrer, 59155 Faches-Thumesnil</h2>
-                    <p className='descriptionP'>Venez déguster nos excellents burgers !
-                        Dans un cadre très décontracté...</p>
-
+                    <Row>
+                        <h1 className='descriptionH1'>Notre <strong>restaurant</strong> de <strong>burgers</strong> à <strong>Faches-Thumesnil</strong></h1>
+                        <h2 className='descriptionH2'>36 rue Ferrer, 59155 Faches-Thumesnil</h2>
+                        <p className='descriptionP'>Venez déguster nos excellents burgers !
+                            Dans un cadre très décontracté...</p>
+                        <Image fluid src={rsburger}></Image>
+                    </Row>
 
                     <div className="google-map">
                         <iframe title="googlemap" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d502.4007306551175!2d3.065083387733846!3d50.60963599385257!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7a5e389e5b1a4036!2sRS%20Burger!5e0!3m2!1sfr!2sfr!4v1642083753686!5m2!1sfr!2sfr" width="100%" height="450" frameborder="0" style={{ border: 0 }} allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
